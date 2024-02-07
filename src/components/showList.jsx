f84da27295a0b2d19ba751072281f8c0d218311f
Fynn Arthur <afynn09@gmail.com>
2024-02-07 10:01:06 +0000

    added search functionality

--- src/components/showList.jsx
+++ src/components/showList.jsx
@@ -6,11 +6,14 @@ import { useStore } from '../store/store'
 const ShowList = (props) => {
     const {shows,isLoading}=props
     const genre=useStore(store=>store.genre)
+    const searchValue=useStore(store=>store.searchTerm)
     const numShows=Array(200).fill(1)
     let showItems=shows
     if(genre){
-      // console.log(genre)
       showItems=shows?.filter(items=>items?.genres.join(', ').includes(genre))
+    }else if(searchValue){
+      console.log(searchValue)
+      showItems=shows?.filter(items=> items.name.toLowerCase().includes(searchValue.toLowerCase()))
     }
 
 if(isLoading){
